1f5410f2ae5fc673fd009dbda86751e57ff8f023
Zoom <zoomfo@gmail.com>
2024-11-07 09:17:21 +0200

    FIX

--- ui-rozetka-tests/src/test/java/tests/home/MathTest.java
+++ ui-rozetka-tests/src/test/java/tests/home/MathTest.java
@@ -1,47 +1,23 @@
 package tests.home;
 
-import com.epam.reportportal.annotations.attribute.Attribute;
-import com.epam.reportportal.annotations.attribute.Attributes;
-import io.qameta.allure.*;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 import static rozetka.config.Config.UI_CONFIGURATION;
 
 public class MathTest {
 
-    @Owner("Serhiy")
-    @Feature("Math operation")
-    @Severity(SeverityLevel.TRIVIAL)
-    @Description("This test checks 2 + 2")
-    @Link(name = "Link", url = "https://github.com/eltess/Mobile-Framefork/tree/master/src/test")
-
-    @Attributes(attributes = {@Attribute(key = "key", value = "value")})
     @Test()
-    public void Sum() throws IOException {
+    public void AAAA()  {
 
-        FileInputStream fileLink = new FileInputStream("src/test/resources/reportportal.properties");
-        Properties prop = new Properties();
-        prop.load(fileLink);
-        System.out.println(prop.getProperty("rp.attributes"));
         System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         System.out.println(UI_CONFIGURATION.getRun());
         System.out.println(UI_CONFIGURATION.getEngine());
         System.out.println(UI_CONFIGURATION.getAuth());
+
         System.out.println(System.getenv("DB_ENGINE"));
         System.out.println(System.getenv("DISABLE_AUTH"));
         System.out.println(System.getenv("RUN"));
+        System.out.println(System.getenv("AAA"));
 
-        var sum = summa();
-        Assert.assertEquals(sum, 4, "Expected sum is different from actual");
-    }
-
-    @Step("Summation in progress {2 + 2}")
-    private int summa() {
-        return 2 + 2;
     }
 }
